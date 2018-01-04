7bb4cafb15a4d44b828505d7cb95084ff1edc568
Solal SABBAH <ssabbah@e1r2p6.42.fr>
2018-01-04 18:30:14 +0100

    abc

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssabbah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/19 17:26:00 by ssabbah           #+#    #+#             */
+/*   Updated: 2017/11/22 10:52:17 by ssabbah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char *)malloc(sizeof(*str) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (i < ft_strlen(s1) + ft_strlen(s2))
+	{
+		str[i] = s2[i - ft_strlen(s1)];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
--- start.c
+++ start.c
@@ -6,7 +6,7 @@
 /*   By: ssabbah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 12:06:26 by ssabbah           #+#    #+#             */
-/*   Updated: 2018/01/04 18:29:59 by ssabbah          ###   ########.fr       */
+/*   Updated: 2018/01/04 17:21:42 by ssabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include "get_next_line.h"
 
 typedef struct	s_param
 {
@@ -23,8 +21,41 @@ typedef struct	s_param
 	void *win;	
 }				t_param;
 
-int	draw(void *mlx, void *win, int x, int y)
+int	ft_key(int keycode, t_param *p)
 {
+	static int x;
+	static int y;
+
+	printf("key event %d\n", keycode);
+	x = 1 + x;
+	y = 1 + y;
+	if (keycode == 125)
+		mlx_pixel_put(p->mlx, p->win, 370, 210 + y, 0xFFFFFF);
+	if (keycode == 126)
+		mlx_pixel_put(p->mlx, p->win, 370, 210 - y, 0xFFFFFF);
+	if (keycode == 124)
+		mlx_pixel_put(p->mlx, p->win, 370 + x, 210, 0xFFFFFF);
+	if (keycode == 123)
+		mlx_pixel_put(p->mlx, p->win, 370 - x, 210, 0xFFFFFF);
+	if (keycode == 53)
+		exit(0);
+	return (0);
+}
+
+int	ft_mouse_hook(int button, int x, int y, t_param *p)
+{
+	printf("mouse event (%d:%d)\n",x, y);
+	return (0);
+}
+
+int	draw(void *mlx, void *win)
+{
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+	printf("%p\n", mlx);
 	while (x < 800)
 	{
 		while (y < 400) 
@@ -41,68 +72,15 @@ int	draw(void *mlx, void *win, int x, int y)
 	return (0);
 }
 
-int	ft_key(int keycode, t_param *p)
+int	main()
 {
-	int x;
-	int y;
+	t_param p;
 
-	x = 0;
-	y = 0;
-	printf("key event %d\n", keycode);
-	if (keycode == 53)
-		exit(0);
-	if (keycode == 126)
-		x = x + 10;
-//	draw(p->mlx, p->win, x, y);
-	return (0);
-}
-
-int	ft_mouse_hook(int button, int x, int y, t_param *p)
-{
-	printf("mouse event (%d:%d)\n",x, y);
-	return (0);
-}
-
-int	main(int ac, char **av)
-{
-	t_param	p;
-	char	*line;
-	int		x;
-	int		y;
-	int		fd;
-	int		a;
-	int		b;
-
-	y = 0;
-	a = 0;
-	b = 0;
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-		return (0);
 	p.mlx = mlx_init();
-	p.win = mlx_new_window(p.mlx, 800, 800, "fdf");
-	while (get_next_line(fd, &line)) 
-	{
-		x = 0;
-		while (line[x])
-		{
-			if (line[x] == '0' && line[x + 1] == ' ')
-			{	
-				while (a < 1000)
-				{
-					mlx_pixel_put(p.mlx, p.win, x + a, y, 0x269D8F);
-					printf("%d\n", a);
-					a++;
-				}
-			}
-			if (line[x]  '0')
-				mlx_pixel_put(p.mlx, p.win, x, y, 0xFF0000);
-			x++;
-		}
-		y++;
-	}
+	p.win = mlx_new_window(p.mlx, 800, 400, "fdf");
+	draw(p.mlx, p.win);
 	mlx_key_hook(p.win, ft_key, &p);
-	mlx_mouse_hook(p.win, ft_mouse_hook, &p);
+	mlx_mouse_hook(p.win, ft_mouse_hook, &p);	
 	mlx_loop(p.mlx);
 	return (0);
 }
